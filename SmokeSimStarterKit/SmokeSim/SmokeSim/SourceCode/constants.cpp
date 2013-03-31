f143229945bec572f6d636cd5a8bdea49d60a0ae
Michael Rivera <mrivera.lee@gmail.com>
2013-03-31 03:35:45 -0400

    Added smoke sim base code

---
+++ SmokeSimStarterKit/SmokeSim/SmokeSim/SourceCode/constants.cpp
@@ -0,0 +1,15 @@
+// Written by Peter Kutz.
+
+#include "constants.h"
+
+// TODO: Adjust the various settings below, and add more if you want.
+
+const int theMillisecondsPerFrame = 10;
+
+	//TOY CASE
+//const int theDim[3] = {2, 2, 1};
+const int theDim[3] = {40, 40, 1};
+//	const int theDim[3] = {25, 25, 3};
+
+//const double theCellSize = 1.0; 
+const double theCellSize = 0.5;
